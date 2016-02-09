d7423e9e6a53bc7c17a4cdb5090a86cd2a62c211
Michael Horsfield <P13216378@GWHXX584LIN015W.lec-admin.dmu.ac.uk>
2016-02-09 13:34:08 +0000

    helloWorld.c

---
+++ helloWorld.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main() {
+   printf("Hello World\n");
+}
+ 
+Save, add and commit helloWorld.c with an appropriate message. Now 
+modify the program so it is reads
+#include<stdio.h>
+main() {
+   printf("Hello World\n");
+   printf(�Hello You!\n�);
+}
+ 
